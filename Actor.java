c1024b1d4463f363d131de3e042878079e3e3a4c
Am4n2503 <113177922+Am4n2503@users.noreply.github.com>
2024-08-21 13:15:02 +0530

    Delete Actor.java

--- Actor.java
+++
@@ -1,9 +0,0 @@
-package StreamAPI;
-public interface Actor{
-    void act();
-    void speak();
-    default void comedy(){//default method can be defined inside an interface//
-        System.out.println("Laugh");
-    }
-}
-
